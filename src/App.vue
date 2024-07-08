c1a26b66356b48441fea722172427e4f85a649be
lkx <18959353261@163.com>
2024-07-08 10:10:07 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <router-view></router-view>
+
+    <tab-bar />
+  </div>
+</template>
+
+<script setup>
+import TabBar from '@/components/TabBar/index.vue';
+</script>
+
+<style scoped></style>
